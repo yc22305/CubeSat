dcdc7874168f104456e476929ccf89d26c22a5e4
yc22305 <fir22305@gmail.com>
2018-06-07 14:06:59 +0800

    successfully send message!

--- arduino/Bluetooth/Bluetooth.ino
+++ arduino/Bluetooth/Bluetooth.ino
@@ -1,6 +1,10 @@
-#include <Wire.h>  
-char message[100]; // to record the message received from Android
-int message_size; 
+#include <Wire.h> 
+
+#define MSG_BUf_LENGTH 64
+
+char message[MSG_BUf_LENGTH]; // to record the message received from Android
+int msg_size;
+int msg_get;
 
 void setup() {
   Serial.begin(9600); //Arduino baud rate is 9600 
@@ -8,15 +12,18 @@ void setup() {
 }
 
 void loop() { 
-  message_size = Serial1.available();
-  if (message_size > 0) { 
+  msg_get = Serial1.available(); // to check if there is data
+  if (msg_get > 0) {
+     delay(50); // leave some time to write data 
+     msg_size = Serial1.available(); // the size of the data
+     for (int i=0; i<MSG_BUf_LENGTH; i++) { // initialization
+         message[i] = 0; 
+        }
      Serial.println("link!");
-     for (int i=0; i<message_size; i++) {
-         Serial.println("for loop");
+     for (int i=0; i<msg_size; i++) {
          message[i] = Serial1.read(); // import one character once 
-         Serial.print(message[i]);
-         if (i == message_size-1)
-             Serial.println(); // the message is over  
+         if (i == msg_size-1)
+             Serial.println(message); // the message is over  
        }
    }
 }

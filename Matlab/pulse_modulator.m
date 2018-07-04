2b58eb6c4f2c427f562719760d72ff052b8cf2c6
yc22305 <fir22305@gmail.com>
2018-07-05 02:17:53 +0800

    set the angle representation to -180~180; while around -180 and 180, set the angle representation to 180~360 for solving discontinuity

--- Matlab/pulse_modulator.m
+++ Matlab/pulse_modulator.m
@@ -7,9 +7,9 @@ Dev=sqrt(Var)/180*pi;
 
 %% Initialization
 %%%%%%%%%%%%%% time %%%%%%%%%%%%%%
-sensor_period=0.2;
+sensor_period=0.1;
 drawing_period=0.01; 
-end_time=50;
+end_time=60;
 
 testing_period=0:drawing_period:end_time;
 data_samplingNumber=size(testing_period,2);
@@ -24,14 +24,7 @@ l=0.464; %length meter
 I=1/12*m*(w^2+l^2); %moment of inertia (kg*meter^2)
 Sys=tf(1,[I 0]); %system transfer function
 angu_v_initial=0/180*pi; %rad/sec
-angle_initial=30/180*pi; %rad.
-if angle_initial>pi
-    angle_initial=rem(angle_initial,2*pi)-2*pi;
-else
-    if angle_initial<-pi
-        angle_initial=rem(angle_initial,2*pi)+2*pi;
-    end
-end
+angle_initial=30/180*pi; %rad. Must between -180~180
 
 %%%%%%%%%%%%% thruster %%%%%%%%%%%%%%
 Sys_thrust=tf(1,1); %tf(4800,[1 140 4800]); %thruster perturbation
@@ -48,14 +41,22 @@ K_angu_v=0.6; %feedback gain for angular velocity
 %%%%%%%% controller %%%%%%%%
 uplimit=thrust_M_design;
 %angle_threshold=0.5/180*pi;
-deadband=uplimit*0.2; %angle_threshold*K_angle;
+deadband=uplimit*0.25; %angle_threshold*K_angle;
 
 %%%%%%%%%%%%%% disturbance value %%%%%%%%%%%%%%
 M_passive_dist_value=0.01764;
 
 %%%%%%%%%%%%%% expectation and user input %%%%%%%%%%%%%%
 Expectation=0; % the desired value for the control loop input.  
-Desired_angle=10/180*pi; %the orientation we want to achieve.
+Desired_angle_input = 0/180*pi; %the orientation we want to achieve. Must between -180~180
+Range_adj = 175/180*pi; % range for adjustment to make the angles around 180 and -180 continuous
+
+%%%%%%%% Adjustment: to make the angles around 180 and -180 continuous %%%%%%%%
+if Desired_angle_input < -Range_adj 
+    Desired_angle = Desired_angle_input+2*pi;
+else
+    Desired_angle = Desired_angle_input;
+end
 
 %% control loop
 Control_value_record=zeros(1,data_samplingNumber); %to record the control value during testing perioed.
@@ -75,19 +76,40 @@ for i=1:data_samplingNumber
 end
 
 %%%%%%%%%%%%%%% active disturbance %%%%%%%%%%%%%%%%%
-% disturbance_impulse(1,5/drawing_period+1)=10;
-% disturbance_impulse(1,10/drawing_period+1)=-8;
+% disturbance_impulse(1,5/drawing_period+1)=20;
+% disturbance_impulse(1,10/drawing_period+1)=-50;
 % 
 % angle_response=lsim(Sys/s,disturbance_impulse,testing_period,'zoh')'+angle_response;
 % angu_v_response=lsim(Sys,disturbance_impulse,testing_period,'zoh')'+angu_v_response;
 % angle_response_thrust=lsim(Sys/s,disturbance_impulse,testing_period,'zoh')'+angle_response_thrust;
 % angu_v_response_thrust=lsim(Sys,disturbance_impulse,testing_period,'zoh')'+angu_v_response_thrust;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Constrain the angle representation between -180 and 180 
+angle_response(angle_response>pi) = rem(angle_response(angle_response>pi),2*pi)-2*pi;
+angle_response(angle_response<-pi) = rem(angle_response(angle_response<-pi),2*pi)+2*pi;
+angle_response_thrust(angle_response_thrust>pi) = rem(angle_response_thrust(angle_response_thrust>pi),2*pi)-2*pi;
+angle_response_thrust(angle_response_thrust<-pi) = rem(angle_response_thrust(angle_response_thrust<-pi),2*pi)+2*pi;
+% data for sensor to read
+angle_response_adj = angle_response;
+angle_response_thrust_adj = angle_response_thrust;
+if Desired_angle_input < -Range_adj || Desired_angle_input > Range_adj % adjust -180~0 to 180~360 
+    angle_response_adj(angle_response<0) = angle_response(angle_response<0)+2*pi;
+    angle_response_thrust_adj(angle_response_thrust<0) = angle_response_thrust(angle_response_thrust<0)+2*pi;
+end
+
+%initialized conditions for the pulse modulation algorithm
+duration_on=on_duration_min;
+duration_cycle=duration_on+off_duration_min;
+time_last_on=-duration_cycle;
+time_last_off=-duration_cycle+duration_on;
 
 for sensor_point=1:sensor_samplingNumber
+    
     %%%%%%%%%%%%%% sensor data %%%%%%%%%%%%%%%
-    angle_sensorGet=angle_response(1,(sensor_point-1)*scale_sensor2data+1)-Desired_angle;
+    angle_sensorGet=angle_response_adj(1,(sensor_point-1)*scale_sensor2data+1)-Desired_angle;
+    angle_sensorGet_thrust=angle_response_thrust_adj(1,(sensor_point-1)*scale_sensor2data+1)-Desired_angle+Dev(1,1)*randn(1);     
     angu_v_sensorGet=angu_v_response(1,(sensor_point-1)*scale_sensor2data+1);
-    angle_sensorGet_thrust=angle_response_thrust(1,(sensor_point-1)*scale_sensor2data+1)-Desired_angle+Dev(1,1)*randn(1);
     angu_v_sensorGet_thrust=angu_v_response_thrust(1,(sensor_point-1)*scale_sensor2data+1)+Dev(1,2)*randn(1);
     
     %%%%%%%%%%%%%% disturbance determination %%%%%%%%%%%%%%
@@ -113,16 +135,6 @@ for sensor_point=1:sensor_samplingNumber
     end
     
     %%%%%%%%%%%%%%%%% pulse modulator %%%%%%%%%%%%%%%%%
-    if sensor_point==1 %initialized conditions for the pulse modulation algorithm
-        duration_on=on_duration_min;
-        duration_cycle=thrust_M_design*duration_on/abs(Control_value_thrust);
-        if duration_cycle<duration_on+off_duration_min
-            duration_cycle=duration_on+off_duration_min;
-        end
-        time_last_on=-duration_cycle;
-        time_last_off=-duration_cycle+duration_on;
-    end
-    
     if time_last_on>time_last_off
         if abs(Control_value_thrust)>=Control_value_min
             duration_on=off_duration_min*abs(Control_value_thrust)/(thrust_M_design-abs(Control_value_thrust));
@@ -160,9 +172,21 @@ for sensor_point=1:sensor_samplingNumber
     %%%%%%%%%%%%%% response of the plant %%%%%%%%%%%%%%%
     angle_response=lsim(Sys/s,input,testing_period,'zoh')'+angle_response;
     angu_v_response=lsim(Sys,input,testing_period,'zoh')'+angu_v_response;
-
     angle_response_thrust=lsim(Sys/s,input_thrust,testing_period,'zoh')'+angle_response_thrust;
     angu_v_response_thrust=lsim(Sys,input_thrust,testing_period,'zoh')'+angu_v_response_thrust;
+    
+    % Constrain the angle representation between -180 and 180
+    angle_response(angle_response>pi) = rem(angle_response(angle_response>pi),2*pi)-2*pi;
+    angle_response(angle_response<-pi) = rem(angle_response(angle_response<-pi),2*pi)+2*pi;
+    angle_response_thrust(angle_response_thrust>pi) = rem(angle_response_thrust(angle_response_thrust>pi),2*pi)-2*pi;
+    angle_response_thrust(angle_response_thrust<-pi) = rem(angle_response_thrust(angle_response_thrust<-pi),2*pi)+2*pi;
+    % data for sensor to read
+    angle_response_adj = angle_response;
+    angle_response_thrust_adj = angle_response_thrust;
+    if Desired_angle_input < -Range_adj || Desired_angle_input > Range_adj % adjust -180~0 to 180~360
+        angle_response_adj(angle_response<0) = angle_response(angle_response<0)+2*pi;
+        angle_response_thrust_adj(angle_response_thrust<0) = angle_response_thrust(angle_response_thrust<0)+2*pi;
+    end
     
     %%%%%%%%%%%% (out of control loop) record of the input to the plant %%%%%%%%%%%%%%
     Control_value_record(1,(sensor_point-1)*scale_sensor2data+1:(sensor_point-1)*scale_sensor2data+scale_sensor2data)=Control_value;
@@ -172,7 +196,11 @@ end
 
 figure,
 subplot(3,1,1)
-plot(testing_period,angle_response_thrust/pi*180,'b',testing_period,disturbance_impulse,'g',testing_period,angle_response/pi*180,'r'), grid on, title('angle (degree)'), %,testing_period,disturbance_impulse,'g',testing_period,angle_response/pi*180,'r'
+if Desired_angle_input < -Range_adj || Desired_angle_input > Range_adj % -180 is shifted to 180
+    plot(testing_period,angle_response_thrust/pi*180,'b',testing_period,angle_response_thrust_adj/pi*180,'m',testing_period,disturbance_impulse,'g',testing_period,angle_response/pi*180,'r'), grid on, title('angle (degree)'), %,testing_period,disturbance_impulse,'g',testing_period,angle_response/pi*180,'r'
+else
+    plot(testing_period,angle_response_thrust/pi*180,'b',testing_period,disturbance_impulse,'g',testing_period,angle_response/pi*180,'r'), grid on, title('angle (degree)'),
+end
 % dim = [0.4 0.8 0.1 0.1];
 % C=num2str(K_angle);
 % note='K-angle =';
@@ -197,4 +225,4 @@ plot(testing_period,angle_response_thrust/pi*180,'b',testing_period,disturbance_
 subplot(3,1,2)
 plot(testing_period,angu_v_response_thrust/pi*180,'b',testing_period,angu_v_response/pi*180,'r'), grid on, title('avgular velocity (degree/s)'); %testing_period,angu_v_response/pi*180,'r',
 subplot(3,1,3)
-plot(testing_period,Actuator_record,'b',testing_period,Control_value_record,'r'), grid on, title('control value (torque, N-m)'); %testing_period,Control_value_thrust_record,'m',testing_period,Control_value_record,'r',
+plot(testing_period,Actuator_record,'b'), grid on, title('control value (torque, N-m)'); %testing_period,Control_value_thrust_record,'m',testing_period,Control_value_record,'r',
